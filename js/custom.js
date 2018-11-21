1a183ff0b18d0c145d6840652faede158bf961ea
张栋 <itboyer@163.com>
2018-11-21 17:36:51 +0800

    Site updated: 2018-11-21 17:36:50

--- js/custom.js
+++ js/custom.js
@@ -31,6 +31,8 @@ $(function () {
 		
 	  $('#nav').append(bottom);
 	  $('footer').prepend(top);
+	  $('#site-info').prepend('<div id="particles-js"></div>');
+	  $('#post-info').prepend('<div id="particles-js"></div>');
 	}
 	//点击事件
 	var a_idx=0;jQuery(document).ready(function($){$("body").click(function(e){var a=new Array("富强","民主","文明","和谐","自由","平等","公正","法治","爱国","敬业","诚信","友善");var $i=$("<span/>").text(a[a_idx]);a_idx=(a_idx+1)%a.length;var x=e.pageX,y=e.pageY;$i.css({"z-index":999999999999999999999999999999999999999999999999999999999999999999999,"top":y-20,"left":x,"position":"absolute","font-weight":"bold","color":"#ff6651"});$("body").append($i);$i.animate({"top":y-180,"opacity":0},1500,function(){$i.remove();});});});
@@ -67,6 +69,117 @@ $(function () {
           $(".post-qr-code").hide(); 
           $(this).val("显示");
 		 }
+	});
+	
+	particlesJS("particles-js", {
+	  "particles": {
+		"number": {
+		  "value": 40,
+		  "density": {
+			"enable": true,
+			"value_area": 800
+		  }
+		},
+		"color": {
+		  "value": "#ffffff"
+		},
+		"shape": {
+		  "type": "circle",
+		  "stroke": {
+			"width": 0,
+			"color": "#000000"
+		  },
+		  "polygon": {
+			"nb_sides": 5
+		  },
+		  "image": {
+			"src": "img/github.svg",
+			"width": 100,
+			"height": 100
+		  }
+		},
+		"opacity": {
+		  "value": 0.5,
+		  "random": false,
+		  "anim": {
+			"enable": false,
+			"speed": 1,
+			"opacity_min": 0.1,
+			"sync": false
+		  }
+		},
+		"size": {
+		  "value": 3,
+		  "random": true,
+		  "anim": {
+			"enable": false,
+			"speed": 40,
+			"size_min": 0.1,
+			"sync": false
+		  }
+		},
+		"line_linked": {
+		  "enable": true,
+		  "distance": 150,
+		  "color": "#ffffff",
+		  "opacity": 0.4,
+		  "width": 1
+		},
+		"move": {
+		  "enable": true,
+		  "speed": 3,
+		  "direction": "none",
+		  "random": false,
+		  "straight": false,
+		  "out_mode": "out",
+		  "bounce": false,
+		  "attract": {
+			"enable": false,
+			"rotateX": 600,
+			"rotateY": 1200
+		  }
+		}
+	  },
+	  "interactivity": {
+		"detect_on": "canvas",
+		"events": {
+		  "onhover": {
+			"enable": true,
+			"mode": "grab"
+		  },
+		  "onclick": {
+			"enable": true,
+			"mode": "push"
+		  },
+		  "resize": true
+		},
+		"modes": {
+		  "grab": {
+			"distance": 140,
+			"line_linked": {
+			  "opacity": 1
+			}
+		  },
+		  "bubble": {
+			"distance": 400,
+			"size": 40,
+			"duration": 2,
+			"opacity": 8,
+			"speed": 3
+		  },
+		  "repulse": {
+			"distance": 200,
+			"duration": 0.4
+		  },
+		  "push": {
+			"particles_nb": 4
+		  },
+		  "remove": {
+			"particles_nb": 2
+		  }
+		}
+	  },
+	  "retina_detect": true
 	});
 	
 })
