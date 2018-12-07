35b78c10fc61e72e81120b423d9515494f0d1ddf
张栋 <itboyer@163.com>
2018-12-07 15:56:01 +0800

    Site updated: 2018-12-07 15:55:56

--- shenghuo/mkFileLink.sh
+++ shenghuo/mkFileLink.sh
@@ -4,7 +4,7 @@ echo "$list" | while read line;
 do
  path=$line
  file_path=$ThisPath"/"$path
- echo $file_path">>"
+ echo "生成目录link: "$file_path
  file_name=$(ls -l $file_path | grep ".md" | awk '{print $9}' | grep -v "^$"  | awk '{gsub(".md",".html",$0);print }')
  link_name=$file_path"/link.txt"
  echo $file_name > $link_name
